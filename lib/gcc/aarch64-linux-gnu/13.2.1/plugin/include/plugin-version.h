6b181a1186a348deb14c07fe7445c9c85757e8e4
secuflag <secuflag@ticino.com>
2023-10-22 17:36:42 +0200

    GCC 13.2.1 20231022
     * binutils-2.41
     * gmp-6.3.0
     * mpfr-4.2.1
     * mpc-1.3.1
     * isl-0.26
     * glibc-2.38
     * linux 6.5.8

--- lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "13.2.1";
-static char datestamp[] = "20230930";
+static char datestamp[] = "20231022";
 static char devphase[] = "";
 static char revision[] = "";
 
--- lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/version.h
+++ lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/version.h
@@ -9,7 +9,7 @@
 BASEVER, DATESTAMP, DEVPHASE, and REVISION are defined by the
 Makefile.  */
 
-#define version_string "13.2.1 20230930"
+#define version_string "13.2.1 20231022"
 #define pkgversion_string "(GCC) "
 
 /* This is the location of the online document giving instructions for
