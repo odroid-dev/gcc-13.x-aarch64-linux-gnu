45a2d506572ee2f974a49c915ffae1b164d49244
secuflag <secuflag@ticino.com>
2024-01-02 17:15:06 +0100

    GCC 13.2.1 20240102
     * binutils-2.41
     * gmp-6.3.0
     * mpfr-4.2.1
     * mpc-1.3.1
     * isl-0.26
     * glibc-2.38
     * linux 6.6.9

--- lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/plugin-version.h
+++ lib/gcc/aarch64-linux-gnu/13.2.1/plugin/include/plugin-version.h
@@ -6,7 +6,7 @@
 #define GCCPLUGIN_VERSION  (GCCPLUGIN_VERSION_MAJOR*1000 + GCCPLUGIN_VERSION_MINOR)
 
 static char basever[] = "13.2.1";
-static char datestamp[] = "20231125";
+static char datestamp[] = "20240102";
 static char devphase[] = "";
 static char revision[] = "";
 
